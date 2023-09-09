2573c4d70c2a3a106c6c480e396f0283bec3df9a
skyler <pkphung2001@gmail.com>
2023-09-09 18:10:35 +0700

    Update

--- app/components/tab/Tab.tsx
+++ app/components/tab/Tab.tsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-
 const Tab = ({children}: {children: React.ReactNode}) => {
-  const [currentTabIndex, setCurrentTabIndex] = useState(0)
-  
   return (
     <div>{children}</div>
   )
